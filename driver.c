ed93648a5d23b32b5d14bad365246d821e967748
EmelineLY04 <106303669+EmelineLY04@users.noreply.github.com>
2023-06-06 20:44:35 +0200

    Add files via upload

---
+++ driver.c
@@ -0,0 +1,48 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <time.h>
+#include <sys/mman.h>
+#include <stdbool.h>
+#include "mmap_hw_regs.h"
+#include "led.h"
+#include "dipsw_pio.h"
+#include "key_pio.h"
+#include "pio_reg_in.h"
+#include "pio_reg_out.h"
+#include "pio_reg_inout.h"
+#include "led_gpio.h"
+#include "key_gpio.h"
+#include "driver.h"
+
+void write_pi_reg_inout( unsigned int pio_reg_inout );
+void write_pio_reg_out( unsigned int pio_reg_out );
+unsigned int pio_reg_out;
+unsigned int pio_reg_inout;
+
+void DRIVER_setup();
+
+void DRIVER_setup() {
+	PIO_REG_IN_setup();
+	PIO_REG_OUT_setup();
+	PIO_REG_INOUT_setup();
+}
+
+void write_pio_reg_inout( unsigned int pio_reg_inout ) {
+	printf ("write pio_reg_inout\t%s\n", PIO_REG_INOUT_binary_string( pio_reg_inout ) );
+	PIO_REG_INOUT_write( pio_reg_inout );
+}
+void write_pio_reg_out( unsigned int pio_reg_out ) {
+	printf ("write pio_reg_out\t%s\n", PIO_REG_OUT_binary_string( pio_reg_out ) );
+	PIO_REG_OUT_write( pio_reg_out );
+}
+//FONCTION PRINCIPALE
+void DRIVER_out_write_data( unsigned int data ) {
+    write_pio_reg_out(data);//on met les données dans le registre pio_reg_out
+	write_pio_reg_inout(1);//on met à 1 le registre pi_reg_inout pour dire au hardware que les données ont été placées dans le registre
+	sleep(0.0001042);
+	write_pio_reg_inout(0);
+	sleep(1);
+}
---
+++ main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <time.h>
+#include <sys/mman.h>
+#include <stdbool.h>
+#include "mmap_hw_regs.h"
+#include "led.h"
+#include "dipsw_pio.h"
+#include "key_pio.h"
+#include "pio_reg_in.h"
+#include "pio_reg_out.h"
+#include "pio_reg_inout.h"
+#include "led_gpio.h"
+#include "key_gpio.h"
+#include "driver.h"
+
+int main() {
+	MMAP_open();
+	DRIVER_setup();
+
+	while(1) {
+		char c;
+        printf ("write pio_reg_out\n");
+        scanf(" %c",&c);
+        int i;
+        int bit;
+        printf("Représentation binaire de c = ");
+        for (i = 7; i >= 0; i--) {
+            bit = (c >> i) & 1;
+            printf("%d",bit);
+        }
+        printf("\n");
+        //envoyer bit � la place de c
+        DRIVER_out_write_data(c);
+	}
+	MMAP_close();
+	return 0;
+}
